4cab262f171d4b083d93b4b1b6b979419f2130cf
jdp20z <jmd.palmero@gmail.com>
2023-01-31 12:51:17 +0100

    EJER

---
+++ EJERCICIO_15/main.js
@@ -0,0 +1,20 @@
+const users = [
+    {username: 'ppc90', age: 30, premium: false},
+    {username: 'lu65', age: 24, premium: true},
+    {username: 'maria3', age: 36, premium: false},
+    {username: 'abc123', age: 20, premium: false},
+    {username: 'sergio58', age: 26, premium: true}
+  ];
+  
+  
+  users.map(function(user) {
+    if (user.premium) {
+      console.log('El usuario ' + user.username + ' es premium');
+    }
+  });
+  
+  let usersNoPrem = users.filter(function(objeto) {
+    return !objeto.premium;
+  });
+  
+  console.log('userNoPrem', usersNoPrem);
